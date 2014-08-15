b068e67377be21f6ff5d48bda1b9a0e99f6b0b5f
AntonKolmakov <anton.kolmakov@fishtag.ru>
2014-08-15 17:33:23 +0400

    config for database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,24 +24,4 @@ RSpec.configure do |config|
   config.before do
     ActionMailer::Base.deliveries.clear
   end
-
-  config.before :suite  do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before :each, js: true do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before do
-    DatabaseCleaner.start
-  end
-
-  config.after do
-    DatabaseCleaner.clean
-  end
 end
