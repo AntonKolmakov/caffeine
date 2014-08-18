5f448aa5afc7e5bb08e3875edd3bbd19e11d5304
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-08-18 12:45:01 +0400

    Add UserForm backend

---
+++ app/controllers/casein/user_forms_controller.rb
@@ -0,0 +1,42 @@
+module Casein
+  class UserFormsController < Casein::CaseinController
+    responders :flash, :collection
+    respond_to :html
+
+    expose(:user_forms) { UserForm.order(sort_order(:name)).paginate(page: params[:page]) }
+    expose(:user_form, attributes: :user_form_params)
+
+    def index
+      @casein_page_title = 'User forms'
+    end
+  
+    def edit
+      @casein_page_title = 'View user form'
+    end
+  
+    def new
+      @casein_page_title = 'Add a new user form'
+    end
+
+    def create
+      user_form.save
+      respond_with(:casein, user_form)
+    end
+  
+    def update
+      user_form.save
+      respond_with(:casein, user_form)
+    end
+ 
+    def destroy
+      user_form.destroy
+      respond_with(:casein, user_form)
+    end
+  
+    private
+
+    def user_form_params
+      params.require(:user_form).permit(:name, :email)
+    end
+  end
+end
