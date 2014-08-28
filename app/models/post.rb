503f4a8574df6a1e7ce4038fa80585b8bc9245cc
AntonKolmakov <anton.kolmakov@fishtag.ru>
2014-08-28 12:54:15 +0400

    correction

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,6 @@ class Post < ActiveRecord::Base
   friendly_id :name, use: %i(slugged history)
 
   def should_generate_new_friendly_id?
-    name_changed? || slug_changed? unless fix_slug
+    name_changed? || slug_changed? unless fix_slug? && slug.present?
   end
 end
