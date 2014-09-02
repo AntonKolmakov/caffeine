1706a8af5376cd1d47e14b5294811c7b911c6375
Anton Kolmakov <anton.kolmakov@fishtag.ru>
2014-09-02 06:22:05 +0000

    post_spec.rb edited online with Bitbucket

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,17 +8,17 @@ describe Post do
     let(:post) { build_stubbed(:post, name: 'post') }
     subject { post.should_generate_new_friendly_id? }
 
-    it 'returns false slug is fixed by admin' do
+    it 'returns false when slug is fixed by admin' do
       post.stub(fix_slug?: true)
       expect(subject).to be_false
     end
 
-    it 'returns false slug is not fixed by admin' do
+    it 'returns false when slug is not fixed by admin' do
       post.stub(fix_slug?: false, slug_changed?: true)
       expect(subject).to be_true
     end
 
-    it 'returns true slug fixed by admin and not named' do
+    it 'returns true when slug fixed by admin and not named' do
       post.stub(fix_slug?: true, slug: '')
       expect(subject).to be_true
     end
