9bc8969aeecc468b03c3a3d913ecb10f89e5877d
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-09-11 17:01:51 +0400

    Try workaround for CKEditor

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,5 +1,6 @@
 module Asset
   class Ckeditor < ActiveRecord::Base
+    include ActiveRecord
     include Ckeditor::Orm::ActiveRecord::AssetBase
 
     delegate :url, :current_path, :content_type, to: :data
