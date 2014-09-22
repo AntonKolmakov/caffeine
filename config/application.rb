baa043513ebf773bcc20cdc214135bb81ab34bfe
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-09-22 16:44:51 +0400

    Try to invalidate assets cache once again

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module PromTehnoSert
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    # Version of your assets, change this if you want to expire all your assets.
+    config.assets.version = '1.2'
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
