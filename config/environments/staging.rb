e933b7997fc293c2e1901335f2b180b5812f1906
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-09-23 19:47:32 +0400

    Disable mailer at stagin

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_delivery = false
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.delivery_method = :test
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
