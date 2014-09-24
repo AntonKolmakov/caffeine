c36769a2b0a36cd4b5c2a46799ad76623afb4429
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-09-24 18:32:01 +0400

    Recreate versions when album watermark changed

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,10 +1,19 @@
 class Album < ActiveRecord::Base
   has_many :pages
-  has_many :images, -> { order 'position' }
+  has_many :images, -> { order 'position' }, dependent: :destroy
 
   validates :name, presence: true
 
   acts_as_tree
 
   mount_uploader :watermark, PictureUploader
+
+  # Recreate all attached images versions to apply new watermark
+  after_save :recreate_versions, on: :update, if: -> { changed_attributes[:watermark] }
+
+  protected
+
+  def recreate_versions
+    images.find_each.map { |i| i.picture.recreate_versions! }
+  end
 end
