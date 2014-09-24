282c11be057f7e63070143f79c5cb821f87c514e
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-09-24 18:31:21 +0400

    Add watermarks to every album image

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -18,16 +18,35 @@ class PictureUploader < BaseUploader
   # Create different versions of your uploaded files:
   version :standard do
     process resize_to_limit: [1024, 768]
+    process :watermark, if: :album_image?
   end
 
-  version :thumb do
+  version :thumb, if: :album_image? do
     process resize_to_limit: [160, 120]
+  end
+
+  def album_image?(_)
+    model.is_a? Image
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
+  end
+
+  # Add watermark to big image
+  def watermark
+    watermark = MiniMagick::Image.open(model.album.watermark.file.path)
+
+    manipulate! do |img|
+      img = img.composite(watermark) do |c|
+        c.compose 'Over'
+        c.gravity 'SouthEast'
+      end
+      img = yield(img) if block_given?
+      img
+    end
   end
 
   # Override the filename of the uploaded files:
