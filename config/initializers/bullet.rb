85ed15441cdb5cb7cfc0600aed72de3beac00f98
Danila Smirnov <danila.smirnov@fishtag.ru>
2014-09-30 16:51:44 +0400

    fix: pages hierarchy list in casein

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,6 +1,6 @@
 Rails.application.config.after_initialize do
   Bullet.enable = true
-  Bullet.alert = true
   Bullet.rails_logger = true
   Bullet.console = true
+  Bullet.add_footer = true
 end if defined?(Bullet)
