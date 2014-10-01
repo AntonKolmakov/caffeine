609e0e4bf03b892374b35e8d85339016742df0b0
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-10-01 12:37:26 +0400

    Refactor base page view

--- app/decorators/page_decorator.rb
+++ app/decorators/page_decorator.rb
@@ -6,4 +6,12 @@ class PageDecorator < Draper::Decorator
   def partial_name
     File.exist?("app/views/pages/custom_partials/_#{object.slug}.html.slim") ? object.slug : BASE_PARTIAL_NAME
   end
+
+  def column_size
+    (object.album && object.album.images.any? && object.page_attachments.any?) ? 8 : 16
+  end
+
+  def image_column_size
+    (column_size == 8) ? 4 : 2
+  end
 end
