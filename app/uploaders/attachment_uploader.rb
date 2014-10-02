eb1a7216f0e1a24d1e72740b32e0c7c04a4810d6
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-10-02 16:49:38 +0400

    Save page attachment original filename

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -1,11 +1,15 @@
-# encoding: utf-8
-
 class AttachmentUploader < BaseUploader
+  before :cache, :save_original_filename
+
   def extension_white_list
     %w(pdf doc htm html docx txt zip)
   end
 
   def filename
     original_filename.to_slug.transliterate(:russian).to_s.downcase if original_filename
+  end
+
+  def save_original_filename(file)
+    model.original_filename ||= file.original_filename if file.respond_to?(:original_filename)
   end
 end
