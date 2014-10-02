ce3271e8faa29d57ebc08f6b24da6946c8807993
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-10-02 12:18:39 +0400

    Update user form submission

--- app/controllers/user_forms_controller.rb
+++ app/controllers/user_forms_controller.rb
@@ -1,6 +1,10 @@
 class UserFormsController < ApplicationController
   expose(:user_form)
+  expose(:user_form_submission) { user_form.user_form_submissions.build }
 
   def show
+    user_form_submission.user_form_fields.each do |field|
+      user_form_submission.field_values.build(user_form_field: field)
+    end
   end
 end
--- app/models/user_form_field_value.rb
+++ app/models/user_form_field_value.rb
@@ -1,5 +1,5 @@
 class UserFormFieldValue < ActiveRecord::Base
   belongs_to :user_form_field, inverse_of: :available_values
 
-  delegate :field_type, to: :user_form_field, allow_nil: true
+  delegate :type, to: :user_form_field, allow_nil: true
 end
