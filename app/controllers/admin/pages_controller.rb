bf196b0a6e77e0463ad612e95b781c3315aa7241
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-10-03 11:50:05 +0400

    Fix page update location

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -20,7 +20,7 @@ module Admin
 
     def update
       page.save
-      respond_with(:admin, page)
+      respond_with :admin, page, location: -> { edit_admin_page_path(page) }
     end
 
     def destroy
