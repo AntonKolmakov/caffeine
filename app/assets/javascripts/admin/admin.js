e520f756a202376fed501737bc51d82e18b7fa24
Daniil Klementev <dk.umka@gmail.com>
2014-10-03 18:35:44 +0400

    Admin login styles

---
+++ app/assets/javascripts/admin/admin.js
@@ -0,0 +1,18 @@
+//= require admin/vendor/jquery-1.10.2.min
+//= require admin/vendor/jquery-ui/jquery-ui
+//= require admin/vendor/bootstrap/bootstrap.min
+//= require admin/vendor/amcharts/amcharts
+//= require admin/vendor/amcharts/serial
+//= require admin/vendor/amcharts/pie
+//= require admin/vendor/chart
+//= require admin/vendor/map
+//= require admin/vendor/jquery-jvectormap/jquery-jvectormap-1.2.2.min
+//= require admin/vendor/jquery-jvectormap/jquery-jvectormap-us-aea-en
+//= require admin/vendor/main
+
+//= require jquery-fileupload/basic
+//= require jquery-fileupload/vendor/tmpl
+//= require jquery_nested_form
+//= require ckeditor/init
+//= require ./ckeditor/config
+//= require ./albums
