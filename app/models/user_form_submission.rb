ac79998b0915af18d68718745be39b8601f12886
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-10-03 12:56:00 +0400

    Fix user form rendering and automate it's submission values population

--- app/models/user_form_submission.rb
+++ app/models/user_form_submission.rb
@@ -6,7 +6,24 @@ class UserFormSubmission < ActiveRecord::Base
 
   accepts_nested_attributes_for :field_values
 
+  # We build form field values for each instance of this form submission
+  after_initialize :build_user_form_fields, if: ->(s) { s.new_record? }
+
+  # But we should reject all those instantiated empty fields when we save submission
+  # to prevent duplicated records
+  before_create do |s|
+    field_values.delete(field_values.select { |f| f.field_value.nil? })
+  end
+
   def field_value(field)
     field_values.where(field: field)
+  end
+
+  private
+
+  def build_user_form_fields
+    user_form_fields.each do |field|
+      field_values.build(user_form_field: field)
+    end
   end
 end
