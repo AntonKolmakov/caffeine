115467ce31dff6c2f2fff328211949a42294b334
AntonKolmakov <anton.kolmakov@fishtag.ru>
2014-10-06 14:11:44 +0400

    Move admin theme js files to vendor

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -1,14 +1,12 @@
-//= require admin/vendor/jquery-1.10.2.min
-//= require admin/vendor/jquery-ui/jquery-ui
-//= require admin/vendor/bootstrap/bootstrap.min
-/*//= require admin/vendor/amcharts/amcharts*/
-//= require admin/vendor/amcharts/serial
-//= require admin/vendor/amcharts/pie
-//= require admin/vendor/chart
-//= require admin/vendor/map
-//= require admin/vendor/jquery-jvectormap/jquery-jvectormap-1.2.2.min
-//= require admin/vendor/jquery-jvectormap/jquery-jvectormap-us-aea-en
-/*//= require admin/vendor/main*/
+//= require admin/jquery-1.10.2.min
+//= require admin/jquery-ui/jquery-ui
+//= require admin/bootstrap/bootstrap.min
+//= require admin/amcharts/amcharts
+//= require admin/chart
+//= require admin/map
+//= require admin/jquery-jvectormap/jquery-jvectormap-1.2.2.min
+//= require admin/jquery-jvectormap/jquery-jvectormap-us-aea-en
+//= require admin/main
 
 //= require jquery-fileupload/basic
 //= require jquery-fileupload/vendor/tmpl
