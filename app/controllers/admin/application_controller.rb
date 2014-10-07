10e89872f53587e43ddea4ca768b7f9c65d68793
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-10-07 17:11:04 +0400

    Move all devise logic into admin namespace

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,9 +2,19 @@ module Admin
   class ApplicationController < ::ApplicationController
     responders :location, :flash
 
-    before_action :authenticate_user!
+    before_action :authenticate_admin_user!
 
     layout 'admin/layouts/application'
+
+    protected
+
+    def devise_parameter_sanitizer
+      if resource_class == User
+        User::ParameterSanitizer.new(User, :user, params)
+      else
+        super
+      end
+    end
 
     decent_configuration do
       strategy DecentExposure::StrongParametersStrategy
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,6 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  layout :layout_by_resource
-
-  protected
-
-  def layout_by_resource
-    devise_controller? ? 'admin/layouts/login' : 'application'
-  end
-
-  protected
-
-  def devise_parameter_sanitizer
-    if resource_class == User
-      User::ParameterSanitizer.new(User, :user, params)
-    else
-      super
-    end
-  end
-
   decent_configuration do
     strategy DecoratorStrategy
   end
