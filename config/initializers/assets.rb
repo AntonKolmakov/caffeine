f655596e5a2aedd5d0707ca8ca14b359125fa154
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-10-09 11:24:02 +0400

    Precompile Ckeditor additional assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,5 +5,6 @@ Rails.application.configure do
   config.assets.precompile += %w(vendor/modernizr.js)
   config.assets.precompile += %w(admin/admin.js admin/admin.css) # admin backend assets
   config.assets.precompile += %w(admin/login/login.js admin/login/login.css) # admin login assets
-  config.assets.precompile += Ckeditor.assets
+  config.assets.precompile += Ckeditor.assets # base CKEDITOR assets
+  config.assets.precompile += %w(ckeditor/*) # CKEDITOR additional assets like lang files
 end
