acec03790204d0db89f01c9e57663be751c90847
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-10-09 10:29:14 +0400

    Fix invalid assets precompile argument

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 Rails.application.configure do
   config.assets.version = '1.2'
-  config.assets.precompile += 'vendor/modernizr.js'
+  config.assets.precompile += %w(vendor/modernizr.js)
   config.assets.precompile += %w(admin/admin.js admin/admin.css) # admin backend assets
   config.assets.precompile += %w(admin/login/login.js admin/login/login.css) # admin login assets
   config.assets.precompile += Ckeditor.assets
