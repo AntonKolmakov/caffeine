c980c69c13f24e551d5539ea1bc0fbcdceb33d17
Daniil Klementev <daniil.klementyev@fishtag.ru>
2014-10-10 18:01:28 +0400

    Fix page image creation

--- app/controllers/admin/page_images_controller.rb
+++ app/controllers/admin/page_images_controller.rb
@@ -4,6 +4,7 @@ module Admin
     expose(:page_image, attributes: :image_params)
 
     def create
+      page_image.page = page
       page_image.save
       respond_with :admin, page_image, location: -> { edit_admin_page_path(page) }
     end
