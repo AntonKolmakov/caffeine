61574a2cf5a10e0b43ebca2b849b93ebe2c53af3
AntonKolmakov <anton.kolmakov@fishtag.ru>
2014-10-14 16:34:39 +0400

    allow user update account without provide password

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,7 +1,7 @@
 def admin_session_sign_in(user = nil)
   user ||= FactoryGirl.create(:admin)
 
-  visit '/admin/pages'
+  visit '/admin'
 
   fill_in 'admin_user_email', with: user.email
   fill_in 'admin_user_password', with: user.password
