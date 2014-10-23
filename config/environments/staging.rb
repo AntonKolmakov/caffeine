b5f5f45eca0f80adf32b98123a401917c8a62354
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-10-23 15:54:24 +0400

    Force http as action mailer asset host

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = 'dgkzpq8ta7oep.cloudfront.net'
-  config.action_mailer.asset_host = config.action_controller.asset_host
+  config.action_mailer.asset_host = ['http://', config.action_controller.asset_host].join
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
