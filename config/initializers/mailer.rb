3c91a82b9cc4111aec6ff63102fe512f671d4a45
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-10-23 16:38:28 +0400

    Updat mailer specs

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,4 +1,4 @@
 ActionMailer::Base.instance_eval do
-  self.default from: "\"АНО Промтехносерт\" <noreply@promtehnosert.ru>"
+  self.default from: %("АНО Промтехносерт" <noreply@promtehnosert.ru>)
   self.default_url_options[:host] = app_config.host
 end
