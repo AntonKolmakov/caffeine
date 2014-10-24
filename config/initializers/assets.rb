d1a86e99b84e3444e8ccad47e01b85a8ec64ebc3
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-10-24 16:27:25 +0400

    Recompile assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.configure do
-  config.assets.version = '1.3'
+  config.assets.version = '1.4'
   config.assets.precompile += %w(vendor/modernizr.js)
   config.assets.precompile += Ckeditor.assets # base CKEDITOR assets
   config.assets.precompile += %w(ckeditor/*) # CKEDITOR additional assets like lang files
