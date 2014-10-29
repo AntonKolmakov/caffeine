5489bf2e9f3e80efaf5269554b1ca5eb82fc1d2d
AntonKolmakov <anton.kolmakov@fishtag.ru>
2014-10-29 11:01:42 +0300

    fix offenses

--- app/controllers/admin/changelogs_controller.rb
+++ app/controllers/admin/changelogs_controller.rb
@@ -3,6 +3,6 @@ require 'kramdown'
 module Admin
   class ChangelogsController < Admin::ApplicationController
     expose(:changelog_file) { Rails.root.join('CHANGELOG.md') }
-    expose(:content) { Kramdown::Document.new(File.read(changelog_file)).to_html}
+    expose(:content) { Kramdown::Document.new(File.read(changelog_file)).to_html }
   end
 end
