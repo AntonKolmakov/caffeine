f133752111bc1a207b6cc2504a70bfcd5d5dd1fb
AntonKolmakov <anton.kolmakov@fishtag.ru>
2014-11-25 22:08:17 +0300

    Rename the migration

--- db/migrate/20141125125647_create_bases.rb
+++ db/migrate/20141125125647_create_bases.rb
@@ -1,6 +1,6 @@
-class CreateMenuElementTypes < ActiveRecord::Migration
+class CreateBases < ActiveRecord::Migration
   def change
-    create_table :menu_element_types do |t|
+    create_table :bases do |t|
       t.string :name
       t.string :type
       t.belongs_to :menu_element, index: true
