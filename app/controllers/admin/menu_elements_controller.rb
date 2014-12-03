1f90e03e166e872ba4ccf362c60daa6cc25d0fc1
AntonKolmakov <anton.kolmakov@fishtag.ru>
2014-12-03 19:45:02 +0500

    Multi step with create action

--- app/controllers/admin/menu_elements_controller.rb
+++ app/controllers/admin/menu_elements_controller.rb
@@ -4,8 +4,14 @@ module Admin
     expose(:menu_element, attributes: :element_types_params, model: DynamicMenu::ElementTypes::Base)
 
     def create
-      menu_element.save
-      respond_with :admin, menu_element, location: -> { edit_admin_menu_element_path(menu_element) }
+      menu_element.current_step = session[:order_step]
+      menu_step
+      if menu_element.new_record?
+        render 'new'
+      else
+        session[:order_step] = nil
+        respond_with :admin, menu_element, location: -> { edit_admin_menu_element_path(menu_element) }
+      end
     end
 
     def update
@@ -22,6 +28,19 @@ module Admin
 
     def element_types_params
       params.require(:dynamic_menu_element_types).permit(:title, :album_id, :page_id, :css, :url, :type)
+    end
+
+    def menu_step
+      if menu_element.valid?
+        if params[:back_button]
+          menu_element.previous_step
+        elsif menu_element.last_step?
+          menu_element.save
+        else
+          menu_element.next_step
+        end
+        session[:order_step] = menu_element.current_step
+      end
     end
   end
 end
